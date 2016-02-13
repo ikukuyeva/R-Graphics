27b07025753ae528cc5637e0abf19a89798cd094
unknown <ikukuyeva@gmail.com>
2016-02-12 21:43:45 -0800

    Add dataset to be analyzed in tutorial and placeholder file for R code

---
+++ R Code/healthcare.R
@@ -0,0 +1,4 @@
+### Healthcare data analysis for tutorial
+
+### Step 0: Export as CSV from https://chhs.data.ca.gov/Healthcare/Number-of-Selected-Inpatient-Medical-Procedures-Ca/mdt8-gwyw
+data <- read.csv("./data/Number_of_Selected_Inpatient_Medical_Procedures__California_Hospitals__2005-2014.csv")
