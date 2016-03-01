44ed8b947de3b080e4ea2292f9a2babcf36f39ad
Irina Kukuyeva <ikukuyeva@gmail.com>
2016-02-29 22:38:10 -0800

    Draft of code for TS section

---
+++ R Code/ts.R
@@ -0,0 +1,90 @@
+library(dplyr)
+library(lattice)
+library(ggplot2)
+# library(stringr)
+
+df <- read.csv(
+  file = "Number_of_Selected_Inpatient_Medical_Procedures__California_Hospitals__2005-2014.csv",
+  encoding='UTF-8',
+  stringsAsFactors = FALSE
+  )
+# Per http://stackoverflow.com/questions/33264688/importing-csv-files-with-special-characters
+# tmp <- gsub("\u0096", "", df$Hospital.Name, fixed=TRUE)
+
+names(df)[1] = 'Year'
+# Or directly from https://chhs.data.ca.gov/api/views/mdt8-gwyw/rows.csv?accessType=DOWNLOAD&bom=true
+
+# for(e in iconvlist()) {
+# print(e)
+# print(read.csv(
+#   file = "Number_of_Selected_Inpatient_Medical_Procedures__California_Hospitals__2005-2014.csv",
+#   encoding='UTF-8',
+#   stringsAsFactors = FALSE
+#   )$Hospital.Name[10])
+# print("---")
+# Sys.sleep(1)
+# }
+
+# x <- (read.csv(
+#   file = "Number_of_Selected_Inpatient_Medical_Procedures__California_Hospitals__2005-2014.csv",
+#   encoding='UTF-8',
+#   stringsAsFactors = FALSE
+#   )$Hospital.Name[10])
+
+
+
+df_clean <- subset(
+  x = df, 
+  County != "STATEWIDE"
+  )
+
+### Same:
+df_LA_CABG <- df %>%
+	filter(County == "Los Angeles") %>%
+	filter(Procedure == "CABG") %>%
+	filter(Volume > 0)
+
+df_Cedars <- df_clean %>%
+	filter( Hospital.Name == 'Cedars Sinai Medical Center')
+
+# df_Kaiser <- df_clean %>%
+# 	filter(Hospital.Name == 'Kaiser Foundation Hospital <U+0096> Sunset')
+
+
+
+df_LA_CABG <- df %>%
+	filter( (County == "Los Angeles") & (Procedure == "CABG") & (Volume > 0) )
+
+# filter(df, (County == "Los Angeles") & (Procedure == "CABG")  & (Volume > 0) )
+
+
+# sort(unique(df_clean$County))
+
+### Across hospitals of LA:
+xyplot(Volume ~ Year | Hospital.Name, 
+	data=df_LA_CABG,
+	par.strip.text=list(cex=0.5),
+	type="b",
+	main="Coronary artery bypass grafting (CABG) in LA"
+	)
+
+
+### For one hospital:
+ggplot( data = df_Cedars ) + 
+  geom_line(
+  	aes(x = Year, y = Volume, linetype = Procedure)
+  	) +
+  scale_x_continuous(
+    breaks=seq(2005, 2014, 3)
+    ) +
+  theme_classic() +
+  ggtitle("Volume of Procedures for Cedars Sinai Medical Center")
+  
+
+
+### How many procedures (overall) per County over time:
+yearly <- group_by(df_clean,
+	Year,
+	Procedure
+	)
+
